4ea1efb5547f6842de9c15fdc549fa8fa018f148
torresposso <erictorres.co@gmail.com>
2025-08-14 14:47:17 -0500

    first commit

---
+++ app/app.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="flex flex-col justify-center h-screen">
+    <img class="p-4" src="/lagrancolombia.png" alt="Logo de la Gran Colombia"></img>
+    <div class="mx-auto space-x-4">
+      <button class="btn">Conoce el "inmanifiesto"</button>
+      <button>Firma x LGC</button>
+    </div>
+  </div>
+</template>
+y
---
+++ nuxt.config.ts
@@ -0,0 +1,22 @@
+import tailwindcss from "@tailwindcss/vite";
+
+export default defineNuxtConfig({
+  devServer: {
+    port: 3001    
+  },
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+  css: ['~/assets/css/main.css'],
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+  modules: [
+    '@nuxt/content',
+    '@nuxt/eslint',
+    '@nuxt/image',
+    '@nuxt/scripts',
+    '@nuxt/ui',
+  ],    
+})
